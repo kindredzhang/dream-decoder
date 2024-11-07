9f3033bd17b7abd95ad53bf46b84e6aad26756bb
kindredzhang <kindred.zhang.life@gmail.com>
2024-11-07 18:06:44 +0800

    typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
-const inter = Inter({ 
-  subsets: ["latin"],
-  variable: '--font-inter',
-});
+// const inter = Inter({ 
+//   subsets: ["latin"],
+//   variable: '--font-inter',
+// });
 
 export const metadata: Metadata = {
   title: "DreamScape Oracles",
