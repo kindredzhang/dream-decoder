5ca79466c65837bbb5774115006c4fb712979531
kindredzhang <kindred.zhang.life@gmail.com>
2024-11-08 10:46:33 +0800

    head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import Head from "next/head";
 
 // const inter = Inter({ 
 //   subsets: ["latin"],
@@ -20,6 +21,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <script
+          src="https://beamanalytics.b-cdn.net/beam.min.js"
+          data-token="bc991f18-75d3-4d44-ba8a-2a4d82cc07bf"
+          async
+        ></script>
+      </Head>
       <body 
         className={cn("min-h-screen bg-background font-sans antialiased")}
         suppressHydrationWarning
